84379fc4b8fe6743e0aad672affc4613679748ee
kakaricho3156 <kakaricho1216@gmail.com>
2023-07-03 13:01:21 +0900

    天気予報の途中

---
+++ temp.py
@@ -0,0 +1,50 @@
+def main():
+    # 3都府県のいくつかの駅名とある日の最高気温(単位: ℃)のデータを辞書として持っています
+    weather_information = [
+        {"prefecture": "東京都", "station": "渋谷", "temperature": 6.5},
+        {"prefecture": "東京都", "station": "池袋", "temperature": 7.0},
+        {"prefecture": "東京都", "station": "新橋", "temperature": 7.5},
+        {"prefecture": "大阪府", "station": "梅田", "temperature": 8.2},
+        {"prefecture": "大阪府", "station": "大阪", "temperature": 9.3},
+        {"prefecture": "大阪府", "station": "堺", "temperature": 9.5},
+        {"prefecture": "福岡県", "station": "博多", "temperature": 13.0},
+        {"prefecture": "福岡県", "station": "太宰府", "temperature": 15.0},
+    ]
+
+    # Q1. 全国の平均気温を計算してください(9.5となればOK)
+
+    # print(weather_information[0]["temperature"])
+    # ↑こうすればtemperatureの値を取り出すことができる
+    total_temp = 0.0
+    for weather in weather_information:
+        total_temp += weather["temperature"]
+
+    avg = total_temp / len(weather_information)
+
+    print(avg)
+
+    # Q2. 大阪府のすべての駅名をカンマ区切りで出力してください( '梅田,大阪,堺' となればOK)
+    oosaka_station = ""
+
+    for weather in weather_information:
+        if weather["prefecture"] == "大阪府" and len(oosaka_station) <= len(weather["prefecture"]):
+            oosaka_station += weather["station"] + ","
+        elif weather["prefecture"] == "大阪府":
+            oosaka_station += weather["station"]
+
+    print(oosaka_station)
+
+
+fukuoka_temp = 0.0
+fukuoka_count = 0
+
+for weather in weather_information:
+    if weather["prefecture"] == "福岡県":
+        fukuoka_temp += weather["temperature"]
+
+print(fukuoka_temp)
+# Q3. 福岡県の平均気温を計算してください(14.0となればOK)
+
+
+if __name__ == "__main__":
+    main()
